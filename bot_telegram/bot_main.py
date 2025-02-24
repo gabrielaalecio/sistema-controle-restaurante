222d4f41e814b804d3c1a2bcd0f8e07d8899d992
Diego Campos <dh9010424@gmail.com>
2025-02-23 20:47:01 -0400

    Criando algumas funções do bot e começando a função cadastro.

--- bot_telegram/bot_main.py
+++ bot_telegram/bot_main.py
@@ -1,14 +1,63 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler,filters
+from telegram import Update,InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler,filters, CallbackContext
+import json
 
-# Função para responder ao comando /start
 async def start(update: Update, context) -> None:
-    await update.message.reply_text("Olá! Eu sou um bot simples. Envie-me uma mensagem e eu a repetirei.")
+    await update.message.reply_text(f"Olá {update.message.chat.first_name}.\nSeja bem vindo ao BUG DO SABOR, sou um chat-bot e estou aqui para te ajudar com o seu pedido!\nQualquer dúvida, digite ou clique /ajuda")
+    
+async def ajuda(update: Update, context) -> None:
+    await update.message.reply_text(f"""
+Sou um chat-bot que auxilia os clientes com seus pedidos, basta interagir comigo que eu ajudo no possível.
+Posso cadastrar clientes, mostrar nossos produtos, realizar pedidos entre outras coisas. Digite ou clique em /comandos para ver minhas possíveis interações.                                 
+""")
 
-# Função para repetir mensagens de texto
 async def echo(update: Update, context) -> None:
-    user_message = update.message.text
-    await update.message.reply_text(f"Você disse: {user_message}")
+    user_id = update.message.from_user.id
+
+    if user_id in cadastro_estado:
+        estado = cadastro_estado[user_id]["estado"]
+
+
+
+    await update.message.reply_text(f"Olá, não entendi oque você disse. Use /ajuda ou /comandos para eu poder melhor te auxiliar",reply_to_message_id=update.message.message_id)
+
+async def comandos(update: Update, context) -> None:
+    await update.message.reply_text(f"""
+Estes são os comandos (digite ou clique):
+
+/realizar_pedido
+/consultar_produtos
+/cadastro
+/avaliar
+/comandos
+/ajuda
+""")
+
+async def cadastro(update: Update, context) -> None:
+    user_id = update.message.from_user.id
+    try:
+        with open(file, "r") as arquivo:
+            clientes = json.load(arquivo)
+        if user_id in clientes['id']:
+            await update.message.reply_text(f"Você ja é um usuário cadastrado!")
+            return None
+    except FileNotFoundError:
+        #enviar um email para o proprietário falando sobre tal problema
+        await update.message.reply_text(f"Desculpe-me {update.message.chat.first_name}. Houve um erro inesperado, tente novamente! caso o problema prossiga envie um email para nossa equipe.")
+        return None
+    
+    if user_id in cadastro_estado:
+        await update.message.reply_text("Você está no processo de cadastro. Conclua ou cancele com /cancelar_cadastro para continuar.")
+        return
+    
+    cadastro_estado[user_id] = {"estado": "email"}
+    await update.message.reply_text("Digite seu e-mail:")
+    
+
+
+    
+file = 'clientes.json'
+cadastro_estado = {} #guarda a informação sobre qual etapa de cadastro o  usuário se encontra
 
 # Configuração e execução do bot
 def main():
@@ -17,8 +66,12 @@ def main():
  # Cria a aplicação
     application = Application.builder().token(token).build()
  # Registra os handlers
-    application.add_handler(CommandHandler("start", start)) # Responde ao /start
-    application.add_handler(MessageHandler(filters.TEXT, echo)) # Repete mensagens de texto
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("ajuda", ajuda))
+    application.add_handler(CommandHandler("comandos", comandos))
+    application.add_handler(CommandHandler("cadastro", cadastro))
+    application.add_handler(MessageHandler(filters.TEXT, echo)) 
+
  # Inicia o bot
     print("Bot está rodando...")
     application.run_polling()
